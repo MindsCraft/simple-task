908605bf93bdbf1d41ce655335caf5eb63168474
Tawshif Ahsan Khan <anikphp@gmail.com>
2025-12-31 03:59:39 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.mobileauth',
+  appName: 'mobile-auth-app',
+  webDir: 'dist'
+};
+
+export default config;
